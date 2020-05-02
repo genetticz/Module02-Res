e5c7f788b1e8be66881a2768aa4288fb1c5f57af
genetticz <mr.michaelwitter@gmail.com>
2020-05-01 20:24:47 -0400

    Duplicate word solved

--- modul_02_java_test_b2001/module_02_test/src/main/java/problems/DuplicateWord.java
+++ modul_02_java_test_b2001/module_02_test/src/main/java/problems/DuplicateWord.java
@@ -1,9 +1,17 @@
 package problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
+
 public class DuplicateWord {
+
+    private static ArrayList<String> storage;
 
     public static void main(String[] args) {
         /*
@@ -12,7 +20,36 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
+        System.out.println(findDup(st));
     }
+
+    public static Map<String,Integer> findDup(String words) {
+
+        storage = new ArrayList<>();
+        Map<String,Integer> mapped = new HashMap<>();
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList(words.replace(".","").split(" ")));
+
+        for(int i = 0; i < arr.size(); i++) {
+
+            if (storage.contains(arr.get(i))) {
+                System.out.println("found existing word: " + arr.get(i));
+                mapped.put(arr.get(i),mapped.get(arr.get(i)) + 1);
+            }
+            else{
+                storage.add(arr.get(i));
+                mapped.put(arr.get(i),1);
+            }
+        }
+        return mapped;
+    }
+
+    /*public boolean doesItExist(String word){
+
+        boolean checkComplete = false;
+
+        for(int i =0; i < storage.size(); i++) {
+            if
+        }
+    }*/
 
 }
